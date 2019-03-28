ecba610530c58d9c239ddca9f9e9aa38e8bc202b
zzndb <emricg2@gmail.com>
2019-03-28 22:00:31 +0800

    implement the navigation bar(not elegant, search(not elegant
    add icon for search, navigation bar

--- app/src/main/java/cn/com/zzndb/wallpaper/view/MainActivity.kt
+++ app/src/main/java/cn/com/zzndb/wallpaper/view/MainActivity.kt
@@ -1,7 +1,11 @@
 package cn.com.zzndb.wallpaper.view
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.widget.ImageButton
 import android.widget.ImageView
 import cn.com.zzndb.wallpaper.R
 import cn.com.zzndb.wallpaper.presenter.PresenterImpl
@@ -11,6 +15,7 @@ import org.jetbrains.anko.find
 class MainActivity : AppCompatActivity(), IView{
 
     private val presenter = PresenterImpl()
+    private val mDrawerLayout: DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,19 @@ class MainActivity : AppCompatActivity(), IView{
     }
 
     private fun initView() {
+        val mDrawerLayout = find(R.id.drawer_layout) as DrawerLayout
+        val imageButtonMore = find(R.id.title_more) as ImageButton
+//        val navView = find(R.id.nav_view) as NavigationView
+        val imageButtonSearch = find(R.id.title_search) as ImageButton
+
+        imageButtonMore.setOnClickListener {    // control drawer bar
+            mDrawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        imageButtonSearch.setOnClickListener {
+            startActivity(Intent(this, search::class.java))
+        }
+
         val imageview = find(R.id.imageshow) as ImageView
         doAsync {
             showImage(imageview)
